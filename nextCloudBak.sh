3f6fd36294358e074f46251984becc0a56df9c95
Berardinux <berardinux@gmail.com>
2023-10-01 19:48:37 -0400

    Oct 1, 19:48

--- nextCloudBak.sh
+++ nextCloudBak.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+sudo -u www-data php occ maintenance:mode --on
+
+sudo rsync -Aavx nextcloud/ /LOCATION/nextcloud-backup_`date +"%Y%m%d"`/
+
+tar cfz /LOCATION/nextcloud-backup_DATE.tgz /LOCATION/nextcloud-backup_DATE/
+
+sudo mysqldump --single-transaction -h SERVER -u USER -p nextcloud > nextclouddb-backup_`date +%Y%m%d"`.bak
+
+sudo -u www-data php occ maintenance:mode --off
