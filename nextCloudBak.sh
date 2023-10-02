d2b27644b76b786c8fa57f69a06262a35e844cf1
Berardinux <berardinux@gmail.com>
2023-10-02 07:37:04 -0400

    Oct 2, 07:37

--- nextCloudBak.sh
+++ nextCloudBak.sh
@@ -1,23 +1,84 @@
 #!/bin/bash
+:' 
+Ensure that you've replaced {
+/PATH/TO/YOUR/BACKUP/DIRECTORY,
+/PATH/TO/YOUR/NEXTCLOUD, 
+YOUR_DB_USER, 
+YOUR_DB_PASSWORD }
+with your actual directory and database information.
+'
+# Print ascii art
+
+cat << "EOF"
+        .:okOXNWWMMWWNXOko:.        
+      ONMMMMMMMMMMMMMMMMMMMMWO      
+      0MMMMMMMMMMMMMMMMMMMMMM0      
+      ;MMMMMMMMMMMMMMMMMMMMMM;      
+       MMMMMMMMMMMMMMMMMMMMMM       
+       NMMMMMMMMMMMMMMMMMMMMM       
+       lMMMMMMMMMMMMMMMMMMMMx       
+       .MMMMMMMMMMMMMMMMMMMM.       
+        MMMMMMMMMMMMMMMMMMMM        
+        xMMMMMMMMMMMMMMMMMMO        
+        'MMMMMMMMMMMMMMMMMM;        
+  .;ldO0NMMMMMMMMMMMMMMMMMMN0Odl;.  
+cWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWo
+    dMMMMMMMMMMMMMMMMMMMMMMMMMMx    
+                                    
+       d                    o       
+       ;                    ,       
+       0                    k       
+      lM                    Xl      
+     .MM                    XM.     
+     ;MM,                  ,MM:     
+      ;MMKo,            ,o0MM:      
+        xMMMMKd;.  .;dKMMMMO        
+          cMMMMMMMMMMMMMMl          
+             .MMMMMMMM,             
+                 ..         
+EOF
+
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run this script as root or with sudo."
+    exit 1
+fi
 
 Date=$(date +"%Y%m%d")
-BackupDir="YOUR_BACKUP_DIR/$Date"
-NextcloudDir="YOUR_NEXTCLOUD"
+BackupDir="/PATH/TO/YOUR/BACKUP/DIRECTORY/$Date"
+NextcloudDir="/PATH/TO/YOUR/NEXTCLOUD"
+User="YOUR_DB_USER"
+Passwd="YOUR_DB_PASSWORD"
+Database="localhost"
+Log="$BackupDir/ncScript.log"
 
 # Create the backup directory
-mkdir -p "$BackupDir"
+if [ -d "$BackupDir" ] || [ -d "$Log" ]; then
+    echo "Backup directory $BackupDir or log file $Log already exists." >> "$Log"
+else
+    mkdir -p "$BackupDir"
+    touch "$Log"
+fi
 
 # Enable maintenance mode
 sudo -u www-data php "$NextcloudDir/occ" maintenance:mode --on
 
 # Backup Nextcloud files using rsync
-sudo rsync -Aavx "$NextcloudDir/" "$BackupDir/nextcloud-backup_$Date/"
+if rsync -Aavx "$NextcloudDir/" "$BackupDir/nextcloud-backup_$Date/"; then
+    echo "Nextcloud files backup completed successfully." >> "$Log"
+else
+    echo "Nextcloud files backup failed." >> "$Log"
+fi
 
 # Create a compressed tarball of the Nextcloud backup
 tar cfz "$BackupDir/nextcloud-backup_$Date.tgz" -C "$BackupDir" "nextcloud-backup_$Date/"
 
 # Backup the Nextcloud database
-sudo mysqldump --single-transaction -h SERVER -u USER -pPASSWORD nextcloud > "$BackupDir/nextclouddb-backup_$Date.bak"
+if mysqldump --single-transaction -h $Database -u "$User" -p"$Passwd" nextcloud > "$BackupDir/nextclouddb-backup_$Date.bak"; then
+    echo "Nextcloud database backup completed successfully." >> "$Log"
+else
+    echo "Nextcloud database backup failed." >> "$Log"
+fi
 
 # Disable maintenance mode
 sudo -u www-data php "$NextcloudDir/occ" maintenance:mode --off
